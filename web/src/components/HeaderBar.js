18d543ad6e0d47802c3697c47e3ae4500eaac518
Cheng <jiansheng80@126.com>
2025-04-04 14:18:36 +0800

    页面优化

--- web/src/components/HeaderBar.js
+++ web/src/components/HeaderBar.js
@@ -144,6 +144,7 @@ const HeaderBar = () => {
     },
     {
       text: t('模型列表'),
+      text: t('模型列表'),
       itemKey: 'pricing',
       to: '/pricing',
       icon: <IconPriceTag style={headerIconStyle} />,
