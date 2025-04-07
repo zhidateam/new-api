951cdc5b17de90003c91dd1b7dcb5b2c29cdd3e1
xy3 <120182408@qq.com>
2025-04-07 18:35:24 +0800

    修改build脚本

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# Get the current version from VERSION file
-VERSION=$(cat VERSION)
-
-# Build the Docker image
-echo "Building new-api Docker image with version: $VERSION"
-docker build -t new-api:$VERSION -t new-api:latest .
+echo "Building new-api Docker image..."
+docker build -t new-api:latest .
 
 echo "Build complete!"
