aad28fd2d2c83e949b1262af342e767a358fdcde
xy3 <120182408@qq.com>
2025-05-07 12:49:29 +0800

    new build sh

--- build.sh
+++ build.sh
@@ -4,6 +4,10 @@ echo "Backing up previous latest image (if exists)..."
 docker image inspect new-api:latest >/dev/null 2>&1 && docker tag new-api:latest new-api:backup
 
 echo "Building new-api Docker image..."
-docker build -t new-api:latest .
+# 不限制资源的build
+# docker build -t new-api:latest .
+
+# 限制使用 2 个 CPU 核心，CPU 时间片分配比例为 512
+docker build --cpus=2 --cpu-shares=512 -t new-api:latest .
 
 echo "Build complete!"
